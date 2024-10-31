73a2bb2dea63690ffb787bc004111314eac5ad15
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2024-10-31 13:58:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (98.91%)

--- submissions/0033-search-in-rotated-sorted-array/solution.java
+++ submissions/0033-search-in-rotated-sorted-array/solution.java
@@ -1,37 +1,36 @@
 class Solution {
-    public int BinarySearch(int[] nums, int low, int high, int target){
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
         while(low <= high){
             int mid = (low + high)/2;
-            if(nums[mid] < target){
-                low = mid + 1;
-            }
-            else if(nums[mid] > target){
-                high = mid - 1;
+
+            if(nums[mid] == target){
+                    return mid;
+                } 
+
+            if(nums[low] <= nums[mid]){
+                
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
             }
             else{
-                return mid;
+                
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
             }
         }
+        
         return -1;
+        
     }
-    public int search(int[] nums, int target) {
-        int RotatedIndex = 0;
-        int n = nums.length;
-        for(int i = 0; i < nums.length-1; i++){
-            if(nums[i] > nums[i+1]){
-                RotatedIndex = i;
-            }
-        }
-        int low1 = 0;
-        int high1 = RotatedIndex;
-        int result1 = BinarySearch(nums, low1, high1, target);
-        int low2 = RotatedIndex + 1;
-        int high2 = n-1;
-        int result2 = BinarySearch(nums, low2, high2, target);
-        if(result1 == -1){
-            return result2;
-        }else{
-            return result1;
-        }
-    }
+    
 }
