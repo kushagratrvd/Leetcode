61e70c2fe85a9a1c9ea7417673ed76abe7e11063
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2024-12-29 18:25:11 +0000

    [LeetCode Sync] Runtime - 12 ms (27.37%), Memory - 47.4 MB (38.53%)

--- submissions/1056-capacity-to-ship-packages-within-d-days/solution.java
+++ submissions/1056-capacity-to-ship-packages-within-d-days/solution.java
@@ -1,36 +1,35 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int low=-(int)1e9,sum=0;
-        for(int i : weights) {
-            low = Math.max(low,i);
-            sum+=i;
+        int low = weights[0];
+        int high = weights[0];
+        for(int i=1; i<weights.length; i++){
+            high += weights[i];
+            low = Math.max(low, weights[i]);
         }
-        // low=1;
-        int high=sum;
-        int ans=0;
-        while(low <= high) {
-            int mid = (low+(high-low)/2);
-            if(possible(mid,weights,days)) {
-                ans=mid;
-                high=mid-1;
+        //int ans=0;
+        while(low < high){
+            int mid = (low + high)/2;
+            int NumOfDays = CountDays(weights,mid);
+            if(NumOfDays <= days){
+                high = mid;
+                //ans = mid;
             }
             else{
-                low=mid+1;
+                low = mid + 1;
             }
         }
-        return ans;
+        return low;
     }
-    public boolean possible(int mid,int[] weights,int days) {
-        int noOfDays=1,sum=0;
-        for(int i=0;i<weights.length;i++) {
-            if(sum+weights[i]<=mid) {
-                sum+=weights[i];
-            }
-            else{
-                noOfDays++;
-                sum=weights[i];
+    public int CountDays(int[] weights, int capacity) {
+        int countDays = 0;
+        int sum = 0;
+        for(int i = 0; i < weights.length; i++){
+            sum += weights[i];
+            if(sum > capacity){
+                sum = weights[i];
+                countDays++;
             }
         }
-        return noOfDays<=days;
+        return countDays+1;
     }
 }
