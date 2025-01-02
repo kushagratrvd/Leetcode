730f117a972bbf682b23f1ae254d6b9479922748
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2025-01-02 20:02:58 +0000

    [LeetCode Sync] Runtime - 1 ms (99.88%), Memory - 46.7 MB (11.14%)

--- submissions/0136-single-number/solution.java
+++ submissions/0136-single-number/solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xorr = 0;
+        int ans = 0;
         for(int i = 0; i < nums.length; i++){
-            xorr ^=nums[i];
+            ans = ans ^ nums[i];
         }
-        return xorr;
+        return ans;
     }
 }
