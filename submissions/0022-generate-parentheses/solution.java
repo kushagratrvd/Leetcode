373724abc2eec394c0c52a6990355ba98c669c14
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2025-05-07 19:08:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.1 MB (75.30%)

--- submissions/0022-generate-parentheses/solution.java
+++ submissions/0022-generate-parentheses/solution.java
@@ -1,31 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<>();
-        StringBuilder str = new StringBuilder();
-        addToList(list, 0, 0, str, n);
-        return list;
+        List<String> s = new ArrayList<>();
+        StringBuilder temp = new StringBuilder();
+        Parenthesis(s, temp, 0, 0, n);
+        return s;
     }
 
-    public void addToList(List<String> list, int l, int r, StringBuilder str, int n) {
-        if (l == n && r == n) {
-            list.add(str.toString());
-            return;
+    public void Parenthesis(List<String> s, StringBuilder temp, int left, int right, int n) {
+        if(left == n && n == right){
+            s.add(temp.toString());
         }
-
-        if (l < n) {
-            str.append("("); // Append an open parenthesis
-            addToList(list, l + 1, r, str, n);
-            str.deleteCharAt(str.length() - 1); // Backtrack
+        if(left < n){
+            temp.append("(");
+            Parenthesis(s, temp, left+1, right, n);
+            temp.deleteCharAt(temp.length()-1);
         }
-
-        if (r < l) {
-            str.append(")"); // Append a close parenthesis
-            addToList(list, l, r + 1, str, n);
-            str.deleteCharAt(str.length() - 1); // Backtrack
+        if(right < left && right < n){
+            temp.append(")");
+            Parenthesis(s, temp, left, right+1, n);
+            temp.deleteCharAt(temp.length()-1);
         }
     }
 }
-
