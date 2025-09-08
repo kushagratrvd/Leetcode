b9c00db7e6663c13cee7042c70146148d39242a3
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2025-09-08 18:55:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.8 MB (6.57%)

---
+++ submissions/0784-insert-into-a-binary-search-tree/solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
+        TreeNode temp = root;
+        while(temp != null){
+            if(temp.val < val){
+                if(temp.right == null){
+                    temp.right = new TreeNode(val);
+                    return root;
+                }
+                temp = temp.right;
+            }
+            else{
+                if(temp.left == null){
+                    temp.left = new TreeNode(val);
+                    return root;
+                }
+                temp = temp.left;
+            }
+            
+        }
+        return root;
+    }
+}
