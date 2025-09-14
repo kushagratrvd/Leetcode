2c7190731f7de792e7d09035db49edda1b632903
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2025-09-14 18:25:20 +0000

    [LeetCode Sync] Runtime - 21 ms (75.63%), Memory - 47.5 MB (74.43%)

---
+++ submissions/1006-vowel-spellchecker/solution.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        Set<String> exactWords = new HashSet<>();
+        Map<String, String> caseInsensitive = new HashMap<>();
+        Map<String, String> vowelInsensitive = new HashMap<>();
+        
+        for (String word : wordlist) {
+            exactWords.add(word);
+            String lower = word.toLowerCase();
+            caseInsensitive.putIfAbsent(lower, word);
+            vowelInsensitive.putIfAbsent(devowel(lower), word);
+        }
+        
+        String[] result = new String[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            String query = queries[i];
+            if (exactWords.contains(query)) {
+                result[i] = query;
+            } else {
+                String lower = query.toLowerCase();
+                if (caseInsensitive.containsKey(lower)) {
+                    result[i] = caseInsensitive.get(lower);
+                } else {
+                    String dev = devowel(lower);
+                    result[i] = vowelInsensitive.getOrDefault(dev, "");
+                }
+            }
+        }
+        return result;
+    }
+    
+    private String devowel(String word) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            if ("aeiou".indexOf(c) >= 0) {
+                sb.append('*');
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
+
