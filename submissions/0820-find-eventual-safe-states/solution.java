4c6c09caab711fa34e5b217084291aa9200c8d50
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2025-10-28 14:44:42 +0000

    [LeetCode Sync] Runtime - 4 ms (98.65%), Memory - 55.6 MB (58.48%)

--- submissions/0820-find-eventual-safe-states/solution.java
+++ submissions/0820-find-eventual-safe-states/solution.java
@@ -1,41 +1,38 @@
 class Solution {
+    public boolean dfs(int curr, int[][] graph, boolean[] visited, boolean[] pathVisited,
+    int[] flag){
+        visited[curr] = true;
+        pathVisited[curr] = true;
+        int[] list = graph[curr];
+
+        for(int i=0; i<list.length; i++){
+            int a = list[i];
+            if(!visited[a]){
+                if(dfs(a, graph, visited, pathVisited, flag)) return true;
+            }
+            else if(pathVisited[a]){
+                //pathVisited[a] = false;
+                //flag[a] = 1;
+                return true;
+            }
+        }
+        pathVisited[curr] = false;
+        flag[curr] = 2;
+        return false;
+    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        List<List<Integer>> graph2 = new ArrayList<>();
-        for(int i=0; i<graph.length; i++) graph2.add(new ArrayList<>());
+        boolean[] visited = new boolean[graph.length];
+        boolean[] pathVisited = new boolean[graph.length];
+        int[] flag = new int[graph.length];
         List<Integer> ans = new ArrayList<>();
-        //Set<Integer> set = new HashSet<>();
-        Stack<Integer> q = new Stack<>();
-        int[] ind = new int[graph.length];
         for(int i=0; i<graph.length; i++){
-            int len = graph[i].length;
-            for(int j:graph[i]){
-                graph2.get(j).add(i);
-            }
-            ind[i] = len;
-            if(len == 0){
-                //set.add(i);
-                q.push(i);
-            } 
-        }
-        while(!q.isEmpty()){
-            int curr = q.pop();
-            ans.add(curr);
-            List<Integer> temp = graph2.get(curr);
-            for(int j:temp){
-                ind[j]--;
-                if(ind[j] == 0) q.push(j);
+            if(!visited[i]){
+                dfs(i, graph, visited, pathVisited, flag);
             }
         }
-        /*for(int i=0; i<graph.length; i++){
-            boolean flag = true;
-            for(int j=0; j<graph[i].length; j++){
-                if(set.contains(graph[i][j])) continue;
-                flag = false;
-                break;
-            }
-            if(graph[i].length > 0 && flag) ans.add(i);
-        }*/
-        Collections.sort(ans);
+        for(int i=0; i<flag.length; i++){
+            if(flag[i] == 2) ans.add(i);
+        }
         return ans;
     }
 }
