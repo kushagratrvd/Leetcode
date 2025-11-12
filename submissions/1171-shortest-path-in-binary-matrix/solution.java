ba1629579776f245ccfeb17252292382074dedac
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2025-11-12 12:41:38 +0000

    [LeetCode Sync] Runtime - 17 ms (46.36%), Memory - 47.3 MB (24.77%)

--- submissions/1171-shortest-path-in-binary-matrix/solution.java
+++ submissions/1171-shortest-path-in-binary-matrix/solution.java
@@ -1,18 +1,4 @@
 class Solution {
-    public void dfs(int[][] dist, int[][] grid, int i, int j, int[] dir) {
-        for(int o:dir){
-            for(int p:dir){
-                if(i+o < 0 || j+p < 0 || i+o >= grid.length || j+p >= grid.length || grid[i+o][j+p] == 1) continue;
-                if(i+o == i && j+p == j) continue;
-                int wt = dist[i][j] + 1;
-                if(dist[i+o][j+p] > wt){
-                    dist[i+o][j+p] = wt;
-                    dfs(dist, grid, i+o, j+p, dir);
-                }  
-            }
-        }
-    
-    }
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
@@ -23,8 +9,6 @@ class Solution {
         int[][] dir = {{-1,-1},{-1,0},{-1,1},
                         {0,-1},    {0,1},
                         {1,-1},{1,0},{1,1}};
-        //dfs(dist, grid, 0, 0, dir);
-
         Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{0,0});
         while(!q.isEmpty()){
@@ -36,6 +20,7 @@ class Solution {
                 int ny = y + i[1];
                 if(nx >= 0 && ny >= 0 && nx < n && ny < n && grid[nx][ny] == 0 && dist[nx][ny] > dist[x][y] + 1){
                     dist[nx][ny] = dist[x][y] + 1;
+                    if(nx == n-1 && ny == n-1) return dist[nx][ny]+1;
                     q.offer(new int[]{nx,ny});
                 } 
             }
