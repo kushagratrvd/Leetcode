a6a8a1ca72e6bcc9cb63fc76735143982f40328a
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2025-12-08 18:03:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.4 MB (37.96%)

--- submissions/0104-maximum-depth-of-binary-tree/solution.java
+++ submissions/0104-maximum-depth-of-binary-tree/solution.java
@@ -14,12 +14,18 @@
  * }
  */
 class Solution {
+    public void dfs(TreeNode root, int[] height, int curr) {
+        if(root == null){
+            height[0] = Math.max(height[0],curr);
+            return;
+        }
+        dfs(root.left, height, curr+1);
+        dfs(root.right, height, curr+1);
+    }
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
-
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-
-        return 1 + Math.max(left,right);
+        int[] height = new int[]{0};
+        dfs(root, height, 0);
+        return height[0];
     }
 }
