5e52ee85c4eac5693e5cb596ff3e94144bc2d810
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2025-12-08 08:37:38 +0000

    [LeetCode Sync] Runtime - 4 ms (38.55%), Memory - 43.5 MB (15.78%)

--- submissions/0020-valid-parentheses/solution.java
+++ submissions/0020-valid-parentheses/solution.java
@@ -1,34 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-
-        Stack <Character> stack = new Stack();
-
+        Stack<Character> st = new Stack<>();
         for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
+            if(!st.isEmpty()){
+                if(s.charAt(i) == ')'){
+                    if(st.pop() != '(') return false; 
+                    continue;
+                } 
+                else if(s.charAt(i) == '}'){
+                    if(st.pop() != '{') return false; 
+                    continue;
+                }
+                else if(s.charAt(i) == ']'){
+                    if(st.pop() != '[') return false; 
+                    continue;
+                }
             }
             
-            else if (c == ')' || c == '}' || c == ']') {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                if(stack.peek() == '(' && s.charAt(i) !=')'){
-                    return false;
-                }
-                else if(stack.peek() == '{' && s.charAt(i) !='}'){
-                    return false;
-                }
-                else if(stack.peek() == '[' && s.charAt(i) !=']'){
-                    return false;
-                }
-                else{
-                    stack.pop();
-                }
-            }
+            st.push(s.charAt(i));
         }
-        return stack.isEmpty();
-
+        if(!st.isEmpty()) return false;
+        return true;
     }
 }
