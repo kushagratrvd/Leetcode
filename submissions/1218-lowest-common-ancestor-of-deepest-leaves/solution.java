5f5be8c917c165c2b1542328e9eefff89c049f25
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-01-09 09:44:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.6 MB (87.82%)

---
+++ submissions/1218-lowest-common-ancestor-of-deepest-leaves/solution.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int dfs(TreeNode node, int height, TreeNode[] subtree, int[] max){
+        if(node == null) return height;
+        int n1 = dfs(node.left,height+1,subtree,max);
+        int n2 = dfs(node.right,height+1,subtree,max);
+        if(n1 == n2 && n1 >= max[0]){
+            subtree[0] = node;
+            max[0] = n1;
+        }
+        return Math.max(n1,n2); 
+    }
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        TreeNode[] subtree = new TreeNode[1];
+        int[] max = new int[]{Integer.MIN_VALUE};
+        dfs(root, 0, subtree, max);
+        return subtree[0];
+    }
+}
