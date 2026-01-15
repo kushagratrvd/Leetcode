a8bb2fa77f16e355988b772f1e3046899596402f
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-01-15 19:29:23 +0000

    [LeetCode Sync] Runtime - 17 ms (21.47%), Memory - 49.9 MB (10.59%)

--- submissions/0542-01-matrix/solution.java
+++ submissions/0542-01-matrix/solution.java
@@ -1,29 +1,37 @@
 class Solution {
-    
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
         int[][] ans = new int[m][n];
+        //for(int i=0; i<m; i++) Arrays.fill(ans[i],Integer.MAX_VALUE);
+        int[][] visited = new int[m][n];
         Queue<int[]> q = new ArrayDeque<>();
+        int[][] dir = new int[][]{{-1,0},{0,1},{1,0},{0,-1}};
+        
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(mat[i][j] == 0) q.offer(new int[]{i,j});
-                else ans[i][j] = -1;
+                if(mat[i][j] == 0){
+                    q.offer(new int[]{i,j});
+                }
             }
         }
-        int[][] dir = {{0,1},{1,0},{-1,0},{0,-1}};
+        
         while(!q.isEmpty()){
-            int[] a = q.poll();
-            for(int[] d: dir){
-                int x = a[0]+d[0];
-                int y = a[1]+d[1];
-                if(x < 0 || y < 0 || x >= m || y >= n) continue;
-                if(ans[x][y] == -1){
-                    ans[x][y] = ans[a[0]][a[1]]+1;
-                    q.offer(new int[]{x,y});
+            int[] curr = q.poll();
+            int row = curr[0];int col = curr[1];
+            for(int[] d : dir){
+                int a = row + d[0];
+                int b = col + d[1];
+                if(a < 0 || b < 0 || a >= m || b >= n || visited[a][b] == 1) continue;
+                if(mat[a][b] == 1){
+                    ans[a][b] = ans[row][col] + 1;
+                    //mat[a][b] = 0;
+                    visited[a][b] = 1;
+                    q.offer(new int[]{a,b});
                 }
-            }    
+            }
         }
+                
         return ans;
     }
 }
