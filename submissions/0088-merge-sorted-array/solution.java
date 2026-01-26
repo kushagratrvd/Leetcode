650d24d4b5283dabda07a49fcd882c7691ec8e36
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-01-26 11:58:26 +0000

    [LeetCode Sync] Runtime - 1 ms (24.38%), Memory - 43.8 MB (55.17%)

--- submissions/0088-merge-sorted-array/solution.java
+++ submissions/0088-merge-sorted-array/solution.java
@@ -1,23 +1,35 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1, j=n-1;
-        int k = m+n-1;
-
-        while(i >= 0 && j >= 0){
-            if(nums1[i] > nums2[j]){
-                nums1[k--] = nums1[i--];
-            }
-            else if(nums1[i] < nums2[j]){
-                nums1[k--] = nums2[j--];
+        Queue<Integer> q = new LinkedList<>();
+        int i=0, j=0;
+        while(i<m+n && j<n){
+            if(q.isEmpty()){
+                if(nums2[j] < nums1[i] || (i>=m && nums1[i] == 0)){
+                    if(i < m) q.offer(nums1[i]);
+                    nums1[i] = nums2[j];
+                    i++;
+                    j++;
+                }
+                else i++;
             }
             else{
-                nums1[k--] = nums1[i--];
-
+                if(nums2[j] < q.peek()){
+                    if(i < m) q.offer(nums1[i]);
+                    nums1[i] = nums2[j];
+                    i++;
+                    j++;
+                }
+                else if(nums2[j] >= q.peek()){
+                    if(i < m) q.offer(nums1[i]);
+                    nums1[i] = q.poll();
+                    i++;
+                }
             }
         }
-        
-        while(j >= 0){
-            nums1[k--] = nums2[j--];
+        while(!q.isEmpty()){
+            if(i < m) q.offer(nums1[i]);
+            nums1[i] = q.poll();
+            i++;
         }
     }
 }
