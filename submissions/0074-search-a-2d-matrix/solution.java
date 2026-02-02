894951e144e2278775cc0118c549e286cca03433
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-02-02 07:08:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.9 MB (76.30%)

--- submissions/0074-search-a-2d-matrix/solution.java
+++ submissions/0074-search-a-2d-matrix/solution.java
@@ -11,8 +11,14 @@ class Solution {
             else if(matrix[mid][n-1] < target) low = mid + 1;
             else return true;
         }
-        for(int j = 0; j < n; j++){
-            if(matrix[low][j] == target) return true;
+        int row = low;
+        low = 0;
+        high = n-1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(matrix[row][mid] > target) high = mid - 1;
+            else if(matrix[row][mid] < target) low = mid + 1;
+            else return true;
         }
         return false;
     }
