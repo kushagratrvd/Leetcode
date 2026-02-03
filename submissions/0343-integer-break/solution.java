cd73323a5aa492674857e72d0fac7489a2b70de5
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-02-03 04:53:14 +0000

    [LeetCode Sync] Runtime - 1 ms (31.40%), Memory - 42.3 MB (32.68%)

--- submissions/0343-integer-break/solution.java
+++ submissions/0343-integer-break/solution.java
@@ -1,8 +1,18 @@
 class Solution {
     public int integerBreak(int n) {
-        if(n<=3) return n-1;
-        if(n%3==0) return (int)Math.pow(3,n/3);
-        if(n % 3 > 1) return (int)Math.pow(3,n/3)*(n%3);
-        return (int)Math.pow(3,n/3-1) * 4;
+        //interesting pattern [0,1,2,3,4,6,9,12,18,27,36,..]
+        if(n <= 3) return n-1;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i = 4; i <= n; i++){
+            int product = 0;
+            for(int j = 1; j <= i/2; j++){
+                product = Math.max(product, dp[j] * dp[i-j]);
+            }
+            dp[i] = product;
+        }
+        return dp[n]; 
     }
 }
