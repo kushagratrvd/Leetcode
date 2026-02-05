558e67ddf577a6bdbdc669eadf0123f6a8378592
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-02-05 08:55:30 +0000

    [LeetCode Sync] Runtime - 1 ms (99.59%), Memory - 43.5 MB (67.51%)

--- submissions/0067-add-binary/solution.java
+++ submissions/0067-add-binary/solution.java
@@ -1,75 +1,82 @@
 class Solution {
     public String addBinary(String a, String b) {
         int carry = 0;
-        int m = a.length();
-        int n = b.length();
-        StringBuilder ans = new StringBuilder();
-        int i=0,j=0;
-        for(i=0,j=0; i<m && j<n; i++,j++){
-            char currA = a.charAt(m-i-1); 
-            char currB = b.charAt(n-j-1);
-            if(currA == '1' && currB == '1'){
-                if(carry == 0){
+        int n1 = a.length();
+        int n2 = b.length();
+        int i=n1-1;
+        int j=n2-1;
+        StringBuilder s = new StringBuilder();
+        while(i >= 0 && j >= 0){
+            char first = a.charAt(i);
+            char second = b.charAt(j);
+            if(first == '1' && second == '1'){
+                if(carry == 1){
+                    s.append('1');
+                }
+                else{
                     carry = 1;
-                    ans.append("0");
+                    s.append('0');
                 }
-                else ans.append("1");
             }
-            else if(currA == '1' || currB == '1'){
-                if(carry == 0){
-                    ans.append("1");
+            else if(first == '1' || second == '1'){
+                if(carry == 1){
+                    s.append('0');
+                    //carry = 1;
                 }
                 else{
-                    ans.append("0");
-                } 
+                    s.append('1');
+                    //carry = 0;
+                }
             }
             else{
-                if(carry == 0){
-                    ans.append("0");
+                if(carry == 1){
+                    s.append('1');
+                    carry = 0;
+                }
+                else s.append('0');
+            }
+            i--;
+            j--;
+        }
+        while(i >= 0){
+            char first = a.charAt(i);
+            if(first == '1'){
+                if(carry == 1){
+                    s.append('0');
                 }
                 else{
-                    ans.append("1");
-                    carry = 0;
-                } 
-            }
-        }
-        while(i<m){
-            char currA = a.charAt(m-i-1); 
-            if(currA == '1' && carry == 1){
-                ans.append("0");
-                carry = 1;
-            }
-            else if(currA == '1' && carry == 0){
-                ans.append("1");
+                    s.append('1');
+                }
             }
             else{
-                if(carry == 0) ans.append("0");
-                else{
-                    ans.append("1");
+                if(carry == 1){
+                    s.append('1');
                     carry = 0;
                 }
-            } 
-            i++;     
-        }
-        while(j<n){
-            char currB = b.charAt(n-j-1); 
-            if(currB == '1' && carry == 1){
-                ans.append("0");
-                carry = 1;
+                else s.append('0');
             }
-            else if(currB == '1' && carry == 0){
-                ans.append("1");
+            i--;
+        }
+        while(j >= 0){
+            char first = b.charAt(j);
+            if(first == '1'){
+                if(carry == 1){
+                    s.append('0');
+                }
+                else{
+                    s.append('1');
+                }
             }
             else{
-                if(carry == 0) ans.append("0");
-                else{
-                    ans.append("1");
+                if(carry == 1){
+                    s.append('1');
                     carry = 0;
                 }
-            }  
-            j++;    
+                else s.append('0');
+            }
+            j--;
         }
-        if(carry == 1) ans.append("1");
-        return ans.reverse().toString();
+        if(carry == 1) s.append('1');
+        return s.reverse().toString();
     }
 }
