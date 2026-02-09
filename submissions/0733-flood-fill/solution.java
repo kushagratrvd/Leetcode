8d465292503b799ac4e3aad1a8706efee95dbfca
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-02-09 19:46:31 +0000

    [LeetCode Sync] Runtime - 1 ms (45.60%), Memory - 47.2 MB (9.74%)

--- submissions/0733-flood-fill/solution.java
+++ submissions/0733-flood-fill/solution.java
@@ -1,18 +1,24 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc] == color) return image;
-        int OriginalColor = image[sr][sc];
-        check(image, OriginalColor, sr, sc, color);
+        Queue<int[]> q = new ArrayDeque<>();
+        int m = image.length;
+        int n = image[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int[][] dir = new int[][]{{-1, 0},{0, 1},{1,0},{0,-1}};
+        q.add(new int[]{sr,sc});
+        visited[sr][sc] = true;
+        int startColor = image[sr][sc]; 
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            image[curr[0]][curr[1]] = color;
+            for(int[] d : dir){
+                int row = curr[0] + d[0];
+                int col = curr[1] + d[1];
+                if(row < 0 || col < 0 || row >= m || col >= n || visited[row][col] || image[row][col] != startColor) continue;
+                q.offer(new int[]{row,col});
+                visited[row][col] = true;
+            }
+        }
         return image;
-    }
-    public void check(int[][] image, int OriginalColor, int sr, int sc, int color){
-        if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != OriginalColor) return;
-
-        image[sr][sc] = color;
-        check(image, OriginalColor, sr+1, sc, color);
-        check(image, OriginalColor, sr-1, sc, color);
-        check(image, OriginalColor, sr, sc+1, color);
-        check(image, OriginalColor, sr, sc-1, color);
-
     }
 }
