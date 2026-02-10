73782e1edeb31595cf736480cc6087fbdef8a8dd
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-02-10 15:26:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.8 MB (26.95%)

---
+++ submissions/0091-decode-ways/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    if (s[0] === '0') {
+            return 0;
+        }
+
+    let n = s.length;
+    dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let one = parseInt(s[i - 1]);
+        let two = parseInt(s.substring(i - 2, i));
+
+        if (1 <= one && one <= 9) {
+            dp[i] += dp[i - 1];
+        }
+        if (10 <= two && two <= 26) {
+            dp[i] += dp[i - 2];
+        }
+    }
+
+    return dp[n];   
+};
