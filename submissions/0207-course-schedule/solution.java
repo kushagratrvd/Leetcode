0191577b6cbef6ac1dcbebcf76ec05f6d76343bf
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-02-11 08:13:33 +0000

    [LeetCode Sync] Runtime - 4 ms (92.82%), Memory - 47.5 MB (14.02%)

--- submissions/0207-course-schedule/solution.java
+++ submissions/0207-course-schedule/solution.java
@@ -1,32 +1,33 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+    public boolean dfs(int[] visited,int[] pathVisited, List<List<Integer>> graph, int node){
+        visited[node] = 1;
+        pathVisited[node] = 1;
+        for(int j=0; j<graph.get(node).size(); j++){
+            int curr = graph.get(node).get(j);
+            if(pathVisited[curr] == 1) return true;
+            if(visited[curr] == 0){
+                if(dfs(visited,pathVisited,graph,curr)) return true;
+            } 
+            
+            //pathVisited[curr] = 0;
+        }
+        pathVisited[node] = 0;
+        return false;
+    }
+    public boolean canFinish(int numCourses, int[][] nums) {
         List<List<Integer>> graph = new ArrayList<>();
         for(int i=0; i<numCourses; i++){
             graph.add(new ArrayList<>());
         }
-        int ans = 0;
-        Queue<Integer> q = new ArrayDeque<>();
-        int[] ind = new int[numCourses];
-        for (int i = 0; i < prerequisites.length; i++) {
-            int x = prerequisites[i][0];
-            int y = prerequisites[i][1];
-            ind[y]++;
-            graph.get(x).add(y);
+        for(int i=0; i<nums.length; i++){
+            graph.get(nums[i][1]).add(nums[i][0]);
         }
-        for(int i=0; i<ind.length; i++){
-            if(ind[i]==0) q.offer(i);
+        int[] visited = new int[numCourses];
+        int[] pathVisited = new int[numCourses];
+        for(int i=0; i<numCourses; i++){
+            if(visited[i] == 1) continue;
+            if(dfs(visited,pathVisited,graph,i)) return false;
         }
-        while(!q.isEmpty()){
-            int j = q.poll();
-            ans++;
-            if(ans > numCourses) return false;
-            List<Integer> temp = graph.get(j);
-            for(int i : temp){
-                ind[i]--;
-                if(ind[i] == 0) q.offer(i);
-            }
-        }
-        if(ans == numCourses) return true;
-        return false;
+        return true;
     }
 }
