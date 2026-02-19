643d9a63f80bcc762b272d01c613e316c30bec9a
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-02-19 06:19:02 +0000

    [LeetCode Sync] Runtime - 4 ms (93.28%), Memory - 46.8 MB (65.47%)

--- submissions/0210-course-schedule-ii/solution.java
+++ submissions/0210-course-schedule-ii/solution.java
@@ -1,29 +1,41 @@
 class Solution {
-    public int[] findOrder(int N, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i=0; i<N; i++) graph.add(new ArrayList<>());
-        int[] ind = new int[N]; 
-        for(int[] i:prerequisites){
-            graph.get(i[1]).add(i[0]);
-            ind[i[0]]++;
-        }
-        
-        Queue<Integer> q = new ArrayDeque<>();
-        for(int i=0; i<ind.length; i++){
-            if(ind[i]==0) q.offer(i); 
-        }
-        int[] ans = new int[N];
-        int k=0;
-        while(!q.isEmpty()){
-            int curr = q.poll();
-            ans[k++] = curr;
-            List<Integer> temp = graph.get(curr);
-            for(int i:temp){
-                ind[i]--;
-                if(ind[i] == 0) q.offer(i);
+    public boolean dfs(int[] pathVisited, int[] visited, List<List<Integer>> graph, int node, Deque<Integer> st){
+        visited[node] = 1;
+        pathVisited[node] = 1;
+        List<Integer> relatives = graph.get(node);
+        for(int curr : relatives){
+            if(pathVisited[curr] == 1){
+                return true;
+            }
+            if(visited[curr] != 1){
+                if(dfs(pathVisited,visited,graph,curr,st)) return true;
             }
         }
-        if(k != N) return new int[0];
-        return ans;      
+        pathVisited[node] = 0;
+        st.push(node);
+        return false;
+    }
+    public int[] findOrder(int num, int[][] pre) {
+        int[] ans = new int[num];
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0; i<num; i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int i=0; i<pre.length; i++){
+            graph.get(pre[i][1]).add(pre[i][0]);
+        }
+        Deque<Integer> st = new ArrayDeque<>();
+        int[] visited = new int[num];
+        int[] pathVisited = new int[num];
+        for(int i=0; i<num; i++){
+            if(visited[i] == 1) continue;
+            if(dfs(pathVisited, visited, graph, i, st)) return new int[0];
+        }
+        int[] res = new int[num];
+        int n = st.size();
+        for(int i=0; i<n; i++){
+            res[i] = st.pop();
+        }
+        return res;
     }
 }
