04061c7717903acd66662c8bbffa6e50eacbfae8
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-02-22 09:28:28 +0000

    [LeetCode Sync] Runtime - 2 ms (90.49%), Memory - 44.3 MB (35.47%)

--- submissions/1036-rotting-oranges/solution.java
+++ submissions/1036-rotting-oranges/solution.java
@@ -2,36 +2,34 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
+        Queue<int[]> q = new ArrayDeque<>();
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == 2) q.offer(new int[]{i,j});
             }
         }
-        int[][] dir = new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
+        int[][] dir = new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
         int count = 0;
-        int o = q.size();
+        int size = q.size(); 
         while(!q.isEmpty()){
-            for(int i=0; i<o; i++){
-                int[] polled = q.poll();
-                
-                for(int[] d:dir){
-                    int row = polled[0]+d[0];
-                    int col = polled[1]+d[1];
-                    if(row < 0 || col < 0 || row >= m || col >= n || grid[row][col] != 1) continue;
-                    q.offer(new int[]{row,col});
-                    grid[row][col] = 2;
+            for(int i = 0; i<size; i++){
+                int[] curr = q.poll();
+                for(int[] d : dir){
+                    int nr = d[0] + curr[0];
+                    int nc = d[1] + curr[1];
+                    if(nr < 0 || nc < 0 || nr >= m || nc >= n || grid[nr][nc] != 1) continue;
+                    grid[nr][nc] = 2;
+                    q.offer(new int[]{nr,nc});
                 }
             }
-            o = q.size();
-            count++;
+            if(!q.isEmpty()) count++;
+            size = q.size();
         }
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == 1) return -1;
             }
         }
-        if(count == 0) return 0;
-        return count-1;
+        return count;
     }
 }
