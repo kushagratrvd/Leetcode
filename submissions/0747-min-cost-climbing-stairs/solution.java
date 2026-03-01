44d4ec868c0d420e085304bde3622eb6eabb6cfe
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-03-01 12:35:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.1 MB (34.89%)

--- submissions/0747-min-cost-climbing-stairs/solution.java
+++ submissions/0747-min-cost-climbing-stairs/solution.java
@@ -1,18 +1,10 @@
 class Solution {
-    public int recursion(int i, int[] cost, int[] dp) {
-        if (i >= cost.length) return 0; 
-        if (dp[i] != -1) return dp[i];
-        int oneStep = recursion(i + 1, cost, dp);
-        int twoStep = recursion(i + 2, cost, dp);
-        dp[i] = cost[i] + Math.min(oneStep, twoStep);
-        return dp[i];
-    }
-
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return Math.min(recursion(0, cost, dp), recursion(1, cost, dp));
+        for(int i=2; i<cost.length; i++){
+            int jump1 = cost[i] + cost[i-1];
+            int jump2 = cost[i] + cost[i-2];
+            cost[i] = Math.min(jump1,jump2);
+        }
+        return Math.min(cost[cost.length-1],cost[cost.length-2]);
     }
 }
-
