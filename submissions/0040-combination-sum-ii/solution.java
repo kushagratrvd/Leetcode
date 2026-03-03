2a848552a106fd982a6f4efad60bd1a1118f8221
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-03-03 22:50:14 +0000

    [LeetCode Sync] Runtime - 6 ms (73.60%), Memory - 45.4 MB (49.74%)

--- submissions/0040-combination-sum-ii/solution.java
+++ submissions/0040-combination-sum-ii/solution.java
@@ -1,29 +1,32 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] nums, int target) {
-        Arrays.sort(nums);
-        List<List<Integer>> arr = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        Combi(nums, target, 0, arr, temp);
-        return arr;
-    }
-
-    public void Combi(int[] nums, int target, int ind, List<List<Integer>> arr, List<Integer> temp) {
-        if(target == 0){   
-            arr.add(new ArrayList<>(temp));
-            return;
-        } 
-        
-        for(int i=ind; i<nums.length; i++){
-            if(i>ind && nums[i] == nums[i-1]){
-                continue;
+    public void dfs(int index, int sum, int[] arr, int target, List<Integer> temp, List<List<Integer>> ans, int prev){
+        if(sum == target){
+            List<Integer> temp2 = new ArrayList<>(temp);
+            //Collections.sort(temp2);
+            if(!ans.contains(temp2)){
+                ans.add(temp2);
             }
-            if(nums[i] > target) break;
-            temp.add(nums[i]);
-            Combi(nums, target-nums[i], i+1, arr, temp);
-            temp.remove(temp.size()-1);
+            return;
         }
+        if(index >= arr.length || sum > target) return;
         
-        
-        //Combi(nums, target, ind+1, arr, temp, start);
+        for(int i = index; i<arr.length; i++){
+            if(sum + arr[i] <= target && prev != arr[i]){
+                temp.add(arr[i]);
+                dfs(i + 1, sum + arr[i], arr, target, temp, ans, prev);
+                prev = temp.get(temp.size()-1);
+                temp.remove(temp.size()-1);
+            }
+        }
+            
+    }
+    public List<List<Integer>> combinationSum2(int[] arr, int target) {
+        int sum = 0;
+        for(int num:arr) sum += num;
+        Arrays.sort(arr);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        dfs(0, 0, arr, target, temp, ans, -1);
+        return ans;
     }
 }
