5c15ba6499903ce050b71e6a61cd9fb939574226
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-03-23 20:31:53 +0000

    [LeetCode Sync] Runtime - 1 ms (78.48%), Memory - 46.7 MB (76.30%)

--- submissions/0026-remove-duplicates-from-sorted-array/solution.java
+++ submissions/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,25 +1,13 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        int count = 0;
-        int j=1;
-        /*List<Integer> arrayList = IntStream.of(nums).boxed().collect(Collectors.toList());
-        for(int i = 0; i < n-1; i++){
-            while(arrayList.get(i) == arrayList.get(i+1)){
-                arrayList.remove(i);
-                n--;
-            }
-        }*/
-        //ArrayList<Integer> arr = new ArrayList<Integer>();
-        for(int i = 1; i < n; i++){
-            if(nums[i-1] != nums[i]){
-                nums[j]=nums[i];
-                count++;
-                j++;
+    public int removeDuplicates(int[] arr) {
+        int max = arr[0];
+        int prev = 1;
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] > max){
+                arr[prev++] = arr[i];
+                max = arr[i];
             }
         }
-        //nums = arrayList.stream().mapToInt(Integer::intValue).toArray();
-        System.out.print(nums); 
-        return (j);
+        return prev;
     }
 }
