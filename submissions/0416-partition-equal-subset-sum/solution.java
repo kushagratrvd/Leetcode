c62a5c78943a929a5178e9966b5d3f5ed772c107
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-03-24 06:59:59 +0000

    [LeetCode Sync] Runtime - 89 ms (25.20%), Memory - 123.1 MB (6.09%)

--- submissions/0416-partition-equal-subset-sum/solution.java
+++ submissions/0416-partition-equal-subset-sum/solution.java
@@ -1,26 +1,30 @@
 class Solution {
-    public boolean dfs(int index, int temp, Boolean[][] set, int[] nums, int sum){
-        if(temp == sum) return true;
-        if(index >= nums.length || temp > sum) return false;
-        
-        if(set[index][temp] != null) return set[index][temp];
-        
-        boolean take = dfs(index+1, temp + nums[index], set, nums, sum);
-        boolean notTake = dfs(index+1, temp, set, nums, sum);
-        
-        return set[index][temp] = take || notTake;
+    public boolean helper(int curr, int ind, int sum, int[] nums, Boolean[][] dp){
+        if(curr == sum) return true;
+        if(curr > sum || ind >= nums.length-1 || dp[ind][curr] != null) return dp[ind][curr] = false;
+        //System.out.println("curr: "+ curr+ " ind: "+ ind);
+        //System.out.println("curr + nums[ind+1]: "+ (curr+nums[ind+1])+ " ind+1: "+ (ind+1));
+        while(ind < nums.length-1 && curr + nums[ind+1] > sum){
+            System.out.println(ind);
+            dp[ind][curr] = false;
+            ind++;
+        }
+        if(ind >= nums.length-1) return dp[ind][curr] = false;
+        if(helper(curr + nums[ind+1], ind+1, sum, nums, dp)) return true;
+        return dp[ind][curr] = helper(curr, ind+1, sum, nums, dp);
     }
-    
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int num : nums) sum += num;
         
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+        }
         if(sum % 2 != 0) return false;
+        sum = sum/2;
+        Boolean[][] dp = new Boolean[nums.length][sum*2+1];
+
+        //System.out.println(sum);
+        return helper(nums[0], 0, sum, nums, dp) || helper(0, 0, sum, nums, dp);
         
-        sum /= 2;
-        
-        Boolean[][] set = new Boolean[nums.length][sum + 1];
-        
-        return dfs(0, 0, set, nums, sum);
     }
 }
