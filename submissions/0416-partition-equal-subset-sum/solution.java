046b849b1e3f1b3240691f2680642c9cbe5045b1
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-04-11 08:36:21 +0000

    [LeetCode Sync] Runtime - 815 ms (5.03%), Memory - 283.9 MB (5.04%)

--- submissions/0416-partition-equal-subset-sum/solution.java
+++ submissions/0416-partition-equal-subset-sum/solution.java
@@ -1,30 +1,23 @@
 class Solution {
-    public boolean helper(int curr, int ind, int sum, int[] nums, Boolean[][] dp){
-        if(curr == sum) return true;
-        if(curr > sum || ind >= nums.length-1 || dp[ind][curr] != null) return dp[ind][curr] = false;
-        //System.out.println("curr: "+ curr+ " ind: "+ ind);
-        //System.out.println("curr + nums[ind+1]: "+ (curr+nums[ind+1])+ " ind+1: "+ (ind+1));
-        while(ind < nums.length-1 && curr + nums[ind+1] > sum){
+    public boolean helper(int[] nums, int ind, int sum, int target, Boolean[][] dp){
+        if(sum > target || ind >= nums.length) return dp[ind][sum] = false;
+        if(dp[ind][sum] != null) return false;
+        if(sum == target) return true;
+        while(ind < nums.length-1 && sum + nums[ind] > target){
             System.out.println(ind);
-            dp[ind][curr] = false;
+            dp[ind][sum+nums[ind+1]] = false;
             ind++;
         }
-        if(ind >= nums.length-1) return dp[ind][curr] = false;
-        if(helper(curr + nums[ind+1], ind+1, sum, nums, dp)) return true;
-        return dp[ind][curr] = helper(curr, ind+1, sum, nums, dp);
+        if(helper(nums, ind+1, sum + nums[ind], target, dp)) return true;
+        if(helper(nums, ind+1, sum, target, dp)) return true;
+        return dp[ind][sum] = false;
     }
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        
-        for(int i=0; i<nums.length; i++){
-            sum += nums[i];
-        }
-        if(sum % 2 != 0) return false;
-        sum = sum/2;
-        Boolean[][] dp = new Boolean[nums.length][sum*2+1];
-
-        //System.out.println(sum);
-        return helper(nums[0], 0, sum, nums, dp) || helper(0, 0, sum, nums, dp);
-        
+        for(int i=0; i<nums.length; i++) sum += nums[i];
+        if(sum%2 != 0) return false;
+        Boolean[][] dp = new Boolean[201][20001]; 
+        if(helper(nums, 0, 0, sum/2, dp)) return true;
+        return false;
     }
 }
