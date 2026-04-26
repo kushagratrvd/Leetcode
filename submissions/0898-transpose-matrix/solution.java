bb9c9ff8de9b805e3080a021be7434f1243bb992
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-04-26 19:29:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 47 MB (11.51%)

--- submissions/0898-transpose-matrix/solution.java
+++ submissions/0898-transpose-matrix/solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
-        //int aarr= matrix[0].length;
-        //int arr1= matrix.length;
-        int[][] arr = new int[matrix[0].length][matrix.length];
-        for(int i=0; i < matrix.length; i++){
-            for(int j=0; j < matrix[0].length; j++){
-                arr[j][i]= matrix[i][j];
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] newMat = new int[n][m];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                newMat[j][i] = matrix[i][j];
             }
         }
-        return arr;
+        return newMat;
     }
 }
