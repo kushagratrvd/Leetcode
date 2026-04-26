7202ce146b22b42b7ed32a97315d29216b38f1c5
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-04-26 19:28:19 +0000

    [LeetCode Sync] Runtime - 1 ms (60.32%), Memory - 47.5 MB (37.64%)

--- submissions/0566-reshape-the-matrix/solution.java
+++ submissions/0566-reshape-the-matrix/solution.java
@@ -1,25 +1,20 @@
-
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        int[] arr= new int[r*c];
-        int k=0;
-        int m=mat.length;
-        int l=mat[0].length;
-        if(r*c!=m*l){
-            return mat;
-        }
-        for(int i=0; i<m; i++){
-            for(int j=0; j<l; j++, k++){
-                arr[k]=mat[i][j];
+        if(r*c != mat.length*mat[0].length) return mat;
+        int[] arr = new int[mat.length * mat[0].length];
+        int o = 0;
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[0].length; j++){
+                arr[o++] = mat[i][j];
             }
         }
-        int[][] arr1 = new int[r][c];
-        int p=0;
+        o = 0;
+        int[][] newMat = new int[r][c];
         for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++, p++){
-                arr1[i][j]=arr[p];
+            for(int j=0; j<c; j++){
+                newMat[i][j] = arr[o++];
             }
         }
-        return arr1;
+        return newMat;
     }
 }
