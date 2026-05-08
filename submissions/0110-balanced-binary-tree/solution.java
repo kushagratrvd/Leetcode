edccd4b3eb03dbe1e0c2967a0f66c97514f77faa
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-05-08 18:58:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.6 MB (58.09%)

--- submissions/0110-balanced-binary-tree/solution.java
+++ submissions/0110-balanced-binary-tree/solution.java
@@ -14,23 +14,16 @@
  * }
  */
 class Solution {
-    public boolean isBalanced(TreeNode root) {
-        int ans = recursion(root);
-        if(ans == -1) return false;
-        return true;
+    public int height(TreeNode root){
+        if(root == null) return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+        if(left == -1 || right == -1) return -1;
+        if(left - right > 1 || right - left > 1) return -1;
+        return Math.max(left, right) + 1;
     }
-
-    public int recursion(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
-
-        int leftDepth = recursion(node.left);
-        if(leftDepth == -1) return -1;
-        int rightDepth = recursion(node.right);
-        if(rightDepth == -1) return -1;
-
-        if(Math.abs(leftDepth-rightDepth) > 1) return -1;
-        return Math.max(leftDepth, rightDepth) + 1;
+    public boolean isBalanced(TreeNode root) {
+        if(height(root) == -1) return false;
+        return true;
     }
 }
