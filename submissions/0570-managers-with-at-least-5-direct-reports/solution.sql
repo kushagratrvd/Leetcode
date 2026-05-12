ef59923d1edf8b625f306978346a01ab0db49dc1
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-05-12 22:33:06 +0000

    [LeetCode Sync] Runtime - 350 ms (56.06%), Memory - 0.0B (100.00%)

--- submissions/0570-managers-with-at-least-5-direct-reports/solution.sql
+++ submissions/0570-managers-with-at-least-5-direct-reports/solution.sql
@@ -1,4 +1,5 @@
--- Write your PostgreSQL query statement below
-select result.name from ( select e1.name, e2.managerId, count(e2.managerId) as e3 from Employee e1
-join Employee e2 on e1.id = e2.managerId 
-group by e1.name, e2.managerId ) as result where result.e3 > 4 
+SELECT e.name
+FROM Employee AS e 
+INNER JOIN Employee AS m ON e.id=m.managerId 
+GROUP BY m.managerId 
+HAVING COUNT(m.managerId) >= 5
