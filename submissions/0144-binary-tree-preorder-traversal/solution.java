06884af74845295c0b0c3193ebe03c66a40a3765
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-06-20 08:39:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.1 MB (52.04%)

--- submissions/0144-binary-tree-preorder-traversal/solution.java
+++ submissions/0144-binary-tree-preorder-traversal/solution.java
@@ -15,16 +15,16 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> preorder = new ArrayList<Integer>();
-        if(root == null) return preorder;
-        Stack<TreeNode> st = new Stack<TreeNode>();
-        st.add(root);
+        List<Integer> li = new ArrayList<>();
+        if(root == null) return li;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
         while(!st.isEmpty()){
-            root = st.pop();
-            preorder.add(root.val);
-            if(root.right != null) st.add(root.right);
-            if(root.left != null) st.add(root.left);
+            TreeNode node = st.pop();
+            li.add(node.val);
+            if(node.right != null) st.push(node.right);
+            if(node.left != null) st.push(node.left);
         }
-        return preorder;
+        return li;
     }
 }
