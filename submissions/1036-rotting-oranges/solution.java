cc68631513311893903a65de552d8b73b0beadf4
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-06-29 08:10:23 +0000

    [LeetCode Sync] Runtime - 2 ms (89.80%), Memory - 44.1 MB (56.63%)

--- submissions/1036-rotting-oranges/solution.java
+++ submissions/1036-rotting-oranges/solution.java
@@ -2,34 +2,40 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        Queue<int[]> q = new ArrayDeque<>();
+        Queue<int[]> q = new LinkedList<>();
+        int[][] visited = new int[m][n];
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j] == 2) q.offer(new int[]{i,j});
-            }
-        }
-        int[][] dir = new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
-        int count = 0;
-        int size = q.size(); 
-        while(!q.isEmpty()){
-            for(int i = 0; i<size; i++){
-                int[] curr = q.poll();
-                for(int[] d : dir){
-                    int nr = d[0] + curr[0];
-                    int nc = d[1] + curr[1];
-                    if(nr < 0 || nc < 0 || nr >= m || nc >= n || grid[nr][nc] != 1) continue;
-                    grid[nr][nc] = 2;
-                    q.offer(new int[]{nr,nc});
+                if(grid[i][j] == 2){
+                    q.offer(new int[]{i, j});
+                    visited[i][j] = 1;
                 }
             }
-            if(!q.isEmpty()) count++;
-            size = q.size();
         }
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] == 1) return -1;
+        int[][] dir = new int[][]{{-1,0}, {1, 0}, {0, 1}, {0,-1}};
+        int count = 0;
+        while(!q.isEmpty()){
+            int s = q.size();
+            count++;
+            for(int i=0; i<s; i++){
+                int[] curr = q.poll();
+                for(int[] d:dir){
+                    int a=d[0] + curr[0];
+                    int b = d[1] + curr[1];
+                    if(a >=0 && b >= 0 && a < m && b < n && visited[a][b] != 1 && grid[a][b] != 0){
+                        visited[a][b] = 1;
+                        q.offer(new int[]{a,b});
+                    }
+                }
             }
         }
-        return count;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(visited[i][j] == 0 && grid[i][j] != 0) return -1;
+            }
+        }
+        if(count == 0) return 0;
+        return count-1;
     }
 }
