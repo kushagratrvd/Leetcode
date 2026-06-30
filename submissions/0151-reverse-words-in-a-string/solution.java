2747d467ffd118e4e9518bfc1e594f9b38435730
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-06-30 16:07:01 +0000

    [LeetCode Sync] Runtime - 8 ms (34.58%), Memory - 47 MB (7.58%)

--- submissions/0151-reverse-words-in-a-string/solution.java
+++ submissions/0151-reverse-words-in-a-string/solution.java
@@ -1,16 +1,21 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.trim().split("\\s+");
-        
-        StringBuilder result = new StringBuilder();
-        
-        for (int i = words.length - 1; i >= 0; i--) {
-            result.append(words[i]);
-            if (i != 0) {
-                result.append(' ');
-            }
+        String ans = "";
+        int i=s.length()-1;
+        while(i>=0){
+            while(i>=0 && s.charAt(i) == ' ') i--;
+            if(i<0) break;
+            int j = i;
+            while(j>=0 && s.charAt(j) != ' ') j--;
+            if(ans.length() > 0) ans += " ";
+                String c = s.substring(j+1,i+1);
+                //System.out.println(c.length());
+                //c.trim();
+                ans += c;
+                
+            i = j;
+            
         }
-        
-        return result.toString();
+        return ans;
     }
 }
