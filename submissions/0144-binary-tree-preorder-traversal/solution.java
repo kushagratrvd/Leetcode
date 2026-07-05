908cdcf65b15df0809574116cc50c030d9e0c792
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-07-05 21:23:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.5 MB (15.02%)

--- submissions/0144-binary-tree-preorder-traversal/solution.java
+++ submissions/0144-binary-tree-preorder-traversal/solution.java
@@ -1,30 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> li = new ArrayList<>();
-        if(root == null) return li;
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        while(!st.isEmpty()){
-            TreeNode node = st.pop();
-            li.add(node.val);
-            if(node.right != null) st.push(node.right);
-            if(node.left != null) st.push(node.left);
+        TreeNode curr = root;
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        while(curr != null){
+            if(curr.left == null){
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                
+                TreeNode prev = curr.left;
+                while(prev.right != null && prev.right != curr) prev = prev.right;
+                if(prev.right == null){
+                    prev.right = curr;
+                    ans.add(curr.val);
+                    curr = curr.left;
+                }
+                else{
+                    prev.right = null;
+                    curr = curr.right;
+                }
+            }
         }
-        return li;
+        return ans;
     }
 }
