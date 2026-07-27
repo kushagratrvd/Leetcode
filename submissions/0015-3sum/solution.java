7b4bbd4b3167c0f75df2f599c27666a3750d570e
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-07-27 20:17:35 +0000

    [LeetCode Sync] Runtime - 26 ms (99.63%), Memory - 59 MB (74.78%)

--- submissions/0015-3sum/solution.java
+++ submissions/0015-3sum/solution.java
@@ -2,31 +2,26 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-
-        for(int i = 0; i<nums.length-2; i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
-
-            int j = i+1;
-            int k = nums.length-1;
-
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum == 0){
-                    List<Integer> temp = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k]));
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    while(j < k && nums[j] == nums[j-1]) j++;
-                    while(j < k && nums[k] == nums[k+1]) k--;
-                }
-                else if(sum < 0){
-                    j++;
-                }
+        for(int i=0; i < nums.length-2 && nums[i] <= 0; i++){
+            
+            int low = i+1;
+            int high = nums.length-1;
+            while(low < high){
+                int sum = nums[low] + nums[high] + nums[i];
+                if(sum > 0) high--;
+                else if(sum < 0) low++;
                 else{
-                    k--;
+                    ans.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        while(low < high && nums[low] == nums[low+1]) low++;
+                    
+                    low++;
+                    //if(nums[high] == nums[high-1]){
+                        while(low < high && nums[high] == nums[high-1]) high--;
+                    //}
+                    high--;
                 }
             }
-            
+            while(nums[i] <= 0 && i < nums.length-1 && nums[i] == nums[i+1]) i++;
         }
         return ans;
     }
