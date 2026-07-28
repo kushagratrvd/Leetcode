2a29dd1fc3c900c63fb4ad51eaf524679ba7a613
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-07-28 04:46:44 +0000

    [LeetCode Sync] Runtime - 48 ms (5.60%), Memory - 51.8 MB (5.24%)

--- submissions/0073-set-matrix-zeroes/solution.java
+++ submissions/0073-set-matrix-zeroes/solution.java
@@ -1,34 +1,50 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int col0 = 1;
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
-                if(matrix[i][j] == 0){
-                    matrix[i][0] = 0;
-                    if (j!=0) matrix[0][j] = 0;
-                    else col0 = 0;
+    public void recursion(int row, int col, int[][] mat, int[][] visited){
+        for(int i=row-1; i>=0; i--){
+            if(mat[i][col] == 0 && visited[i][col] == 0){
+                visited[i][col] = 1;
+                recursion(i, col, mat, visited);
+            }
+            mat[i][col] = 0;
+            visited[i][col] = 1;
+        }
+        for(int i=row+1; i<mat.length; i++){
+            if(mat[i][col] == 0 && visited[i][col] == 0){
+                visited[i][col] = 1;
+                recursion(i, col, mat, visited);
+            }
+            mat[i][col] = 0;
+            visited[i][col] = 1;
+        }
+        for(int j=col-1; j>=0; j--){
+            if(mat[row][j] == 0 && visited[row][j] == 0){
+                visited[row][j] = 1;
+                recursion(row, j, mat, visited);
+            }
+            mat[row][j] = 0;
+            visited[row][j] = 1;
+        }
+        for(int j=col+1; j<mat[0].length; j++){
+            if(mat[row][j] == 0 && visited[row][j] == 0){
+                visited[row][j] = 1;
+                recursion(row, j, mat, visited);
+            }
+            mat[row][j] = 0;
+            visited[row][j] = 1;
+        }
+    }
+    public void setZeroes(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] visited = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j] == 0 && visited[i][j] == 0){
+                    visited[i][j] = 1;
+                    recursion(i, j, mat, visited);
                 }
             }
         }
-        for(int i = 1; i < matrix.length; i++){
-            for(int j = 1; j < matrix[i].length; j++){
-                if(matrix[i][j] != 0){
-                    if(matrix[i][0] == 0 || matrix[0][j] == 0){
-                        matrix[i][j] = 0;
-                    }
-                }
-                
-            }
-        }
-        if(matrix[0][0] == 0){
-            for(int j = 0; j < matrix[0].length; j++){
-                matrix[0][j] = 0;
-            }
-        }
-        if(col0 == 0){
-            for(int i = 0; i < matrix.length; i++){
-                matrix[i][0] = 0;
-            }
-        }
+        
     }
 }
