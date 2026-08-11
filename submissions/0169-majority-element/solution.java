ccd832cd32066f0e88da6e762a370e5d74485778
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-08-11 20:04:28 +0000

    [LeetCode Sync] Runtime - 2 ms (70.93%), Memory - 55.7 MB (46.77%)

--- submissions/0169-majority-element/solution.java
+++ submissions/0169-majority-element/solution.java
@@ -1,15 +1,15 @@
 class Solution {
-    public static int majorityElement(int[] nums) {
-        return helper(nums,0,nums[0]);
-    }static int helper(int[] nums, int si, int ref){
-        int c=0;
-        for(int i=si;i<nums.length;i++){
-            if(nums[i]==ref)
-                c++;
-            else
-                c--;
-            if(c==0)
-                return helper(nums,i,nums[i]);
-        }return ref;
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = nums[0];
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == candidate) count++;
+            else count--;
+            if(count == 0){
+                candidate = nums[i];
+                count = 1;
+            }
+        }
+        return candidate;
     }
 }
