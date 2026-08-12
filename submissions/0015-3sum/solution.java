0b45bfdf6abe4f757101d44309fb303adbc9c6ad
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-08-12 13:45:42 +0000

    [LeetCode Sync] Runtime - 30 ms (88.21%), Memory - 53.4 MB (99.75%)

--- submissions/0015-3sum/solution.java
+++ submissions/0015-3sum/solution.java
@@ -2,26 +2,31 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0; i < nums.length-2 && nums[i] <= 0; i++){
-            
+        int i = 0;
+        while(i < nums.length - 2 && nums[i] <= 0){
             int low = i+1;
             int high = nums.length-1;
             while(low < high){
-                int sum = nums[low] + nums[high] + nums[i];
-                if(sum > 0) high--;
-                else if(sum < 0) low++;
-                else{
-                    ans.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                        while(low < high && nums[low] == nums[low+1]) low++;
-                    
-                    low++;
-                    //if(nums[high] == nums[high-1]){
-                        while(low < high && nums[high] == nums[high-1]) high--;
-                    //}
-                    high--;
-                }
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == 0) {
+    ans.add(Arrays.asList(nums[i], nums[low], nums[high]));
+
+    low++;
+    high--;
+
+    while (low < high && nums[low] == nums[low - 1]) {
+        low++;
+    }
+
+    while (low < high && nums[high] == nums[high + 1]) {
+        high--;
+    }
+}
+                else if(sum > 0) high--;
+                else low++;       
             }
-            while(nums[i] <= 0 && i < nums.length-1 && nums[i] == nums[i+1]) i++;
+            i++;
+            while(i < nums.length && nums[i-1] == nums[i]) i++;
         }
         return ans;
     }
