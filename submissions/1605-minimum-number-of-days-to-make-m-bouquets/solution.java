8f79745fe7e5d7ea9169532903471301485517fd
Kushagra Trivedi <kushagratrivedi962@gmail.com>
2026-08-17 23:02:13 +0000

    [LeetCode Sync] Runtime - 17 ms (73.64%), Memory - 82.3 MB (95.18%)

--- submissions/1605-minimum-number-of-days-to-make-m-bouquets/solution.java
+++ submissions/1605-minimum-number-of-days-to-make-m-bouquets/solution.java
@@ -1,40 +1,31 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        if((long) m * k > bloomDay.length) return -1;
-        int ans = 0;
-        int min = bloomDay[0], max = bloomDay[0];
-        for(int i = 0; i < bloomDay.length; i++){
-            min = Math.min(min,bloomDay[i]);
-            max = Math.max(max, bloomDay[i]);
+        if ((long) m * k > bloomDay.length) return -1;
+        int low = bloomDay[0];
+        int high = bloomDay[0];
+        for(int i : bloomDay){
+            low = Math.min(low, i);
+            high = Math.max(high, i);
         }
-        while(min < max){
-            int mid = (min + max)/2;
-            if(possibleBouqets(bloomDay, mid, k ,m)){
-                max = mid;
-                ans = mid;
-            }
-            else{
-                min = mid + 1;
-            }
-        }
-        return min;
-    }
-    public boolean possibleBouqets(int[] arr, int days, int k, int m){
-        int flowers = 0;
-        int bouqets = 0;
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] <= days){
-                flowers++;
-                if(flowers == k){
-                    bouqets++;
-                    flowers = 0;
+        while(low < high){
+            int mid = low + (high-low)/2;
+            int flower = 0;
+            int bouqet = 0;
+            for(int i : bloomDay){
+                if(mid >= i){
+                    flower += 1;
+                    if(flower == k){
+                        bouqet++;
+                        flower = 0;
+                    }
                 }
+                else flower = 0;
             }
-            else{
-                flowers = 0;
-            }
+            if(bouqet >= m) high = mid;
+            else low = mid+1; 
+            //System.out.println("low " +low);
+            //System.out.println("high " +high);
         }
-        return (bouqets >= m);
+        return low;
     }
 }
-
